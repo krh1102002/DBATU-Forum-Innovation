10306889d9de7d8d4b51a18f557d5da82c0a955f
Kunal Hulke <hulke1102002@gmail.com>
2023-07-31 18:06:52 +0530

    resolve

--- src/components/StakeHolders/RAndInstitution.jsx
+++ src/components/StakeHolders/RAndInstitution.jsx
@@ -12,7 +12,7 @@ const RAndInstitution = () => {
         <div className='flex flex-col justify-center items-center py-6 md:px-12 px-6 bg-blue-100 rounded-xl lg:w-3/4 w-11/12'>
           <div className='flex md:flex-row flex-col-reverse background-banner items-center  gap-6'>
             <div className='flex flex-col gap-2'>
-              <h1 className='text-2xl  font-semibold pb-6 text-[#19191d]'>Research Development & Institution</h1>
+              <h1 className='text-2xl  font-semibold pb-6 text-[#19191d]'>Research and Development Institution</h1>
               {ResearchDevelopmentAndInstitution.map((data) =>(
                 <p className='text-normal  text-justify text-[#19191d]'>{data}</p>
                 ))}
